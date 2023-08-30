2a59c642693355f93f37dba3e7c9956b711c6253
Iarlen <iarlensr@hotmail.com>
2023-08-30 00:06:23 -0300

    chore: installing claudinary, form-auto-content, tap, fastify/multipart and setting claudianary environment variables.

--- src/utils/envSchema.ts
+++ src/utils/envSchema.ts
@@ -5,6 +5,9 @@ const envSchema = z.object({
   NODE_ENV: z.string().optional(),
   PORT: z.string().optional(),
   DATABASE: z.string(),
+  CLOUDINARY_NAME: z.string(),
+  CLOUDINARY_API_KEY: z.string(),
+  CLOUDINARY_SECRET: z.string(),
 })
 
 export const env = envSchema.parse(process.env)
